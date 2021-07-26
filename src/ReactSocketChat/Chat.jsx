efcd95b27cf1b5169c20e44de529842774e2891e
Niku_419 <2019198@iiitdmj.ac.in>
2021-07-26 11:07:48 +0530

    :exploding_head: added my refrences

--- src/ReactSocketChat/Chat.jsx
+++ src/ReactSocketChat/Chat.jsx
@@ -1,6 +1,9 @@
 import React,{useState, useRef, useEffect} from 'react'
-import { Container, Form, Button, InputGroup, ListGroup} from "react-bootstrap";
+import { Container, Form, Button, InputGroup, ListGroup, Nav, Navbar} from "react-bootstrap";
 import io from 'socket.io-client'
+// import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram, faGithub, faFacebookF, faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export default function Chat() {
@@ -42,8 +45,8 @@ export default function Chat() {
             <ListGroup variant="flush">
               {chat.map(({ name, message }, index) => (
                 <div key={index}>
-                  <span style={{display: "inline"}} className="text-muted">{message}: </span>
-                  <span style={{display: "inline"}}>{name}</span>
+                  <span style={{display: "inline"}} className="text-muted">{name}: </span>
+                  <span style={{display: "inline"}}>{message}</span>
                 </div>
               ))}
             </ListGroup>
@@ -84,6 +87,24 @@ export default function Chat() {
           </Form>}
         </Container>
       </Container>
+      <Container>
+        <Navbar bg="transparent" fixed="bottom" variant="light" className="d-flex justify-content-between">
+          <div><Navbar.Brand><strong style={{color: "#ffffff", letterSpacing:"0.12rem"}}>Niku419</strong></Navbar.Brand></div>
+          <div>
+            <Nav className="mr-auto">
+              <Nav.Link href="https://github.com/niku419"><FontAwesomeIcon  icon={faGithub} color="#ffffff" /></Nav.Link>
+              <Nav.Link href="https://linkedin.com/niku_419"><FontAwesomeIcon  icon={faLinkedinIn}  color="#ffffff"/></Nav.Link>
+              <Nav.Link href="https://instagram.com/_niku_419"><FontAwesomeIcon  icon={faInstagram}  color="#ffffff"/></Nav.Link>
+              <Nav.Link href="https://www.facebook.com/profile.php?id=100069976086066"><FontAwesomeIcon icon={faFacebookF}  color="#ffffff"/></Nav.Link>
+            </Nav>
+          </div>
+          {/* <Form inline>
+            <Nav className="mr-auto"> 
+              <Nav.Link>made for Hiku<FontAwesomeIcon color="#8d0101" icon={faHeart} /></Nav.Link>
+            </Nav>
+          </Form> */}
+        </Navbar>
+			</Container>
     </>
   )
 }
